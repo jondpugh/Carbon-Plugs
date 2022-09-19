270a1e1eb197484bf6dd76418c1165f424798717
jondpugh <60925021+jondpugh@users.noreply.github.com>
2022-09-19 16:51:48 +0100

    Add files via upload

---
+++ CutTheCrap.cs
@@ -0,0 +1,10 @@
+﻿namespace Carbon.Plugins
+{
+    [Info("CutTheCrap", "JonDPugh", "1.0.0")]
+    [Description("Fertilizer straight from the horses ass")]
+    public class CutTheCrap : CarbonPlugin
+    {
+        object OnHorseDung(BaseRidableAnimal horse)
+        { return ItemManager.CreateByName("fertilizer"); }
+    }
+}
